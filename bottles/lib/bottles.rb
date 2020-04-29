99aba65a3d593c2605bae1ce053acf5d2062b198
Sandi Metz <sandi@sandimetz.com>
2020-04-29 12:20:23 -0400

    wednesday afternoon

--- bottles/lib/bottles.rb
+++ bottles/lib/bottles.rb
@@ -11,10 +11,10 @@ class Bottles
     bottle_number = bottle_number_for(number)
     next_bottle_number = bottle_number_for(bottle_number.successor)
 
-    "#{bottle_number} of beer on the wall, ".capitalize +
-    "#{bottle_number} of beer.\n" +
+    "#{bottle_number.quantity.capitalize} #{bottle_number.container} of beer on the wall, " +
+    "#{bottle_number.quantity} #{bottle_number.container} of beer.\n" +
     "#{bottle_number.action}, " +
-    "#{next_bottle_number} of beer on the wall.\n"
+    "#{next_bottle_number.quantity} #{next_bottle_number.container} of beer on the wall.\n"
   end
 
   def bottle_number_for(number)
@@ -34,10 +34,6 @@ class BottleNumber
   attr_reader :number
   def initialize(number)
     @number = number
-  end
-
-  def to_s
-    "#{quantity} #{container}"
   end
 
   def quantity
